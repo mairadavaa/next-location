171a3cb16f8cf0b5e1fee4098547307182072239
mairadavaa <114126275+mairadavaa@users.noreply.github.com>
2023-03-20 19:49:51 +0800

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCW28uqcD5YDYltZj9dvJwCiLjrmmG474o",
-    authDomain: "bus-mai.firebaseapp.com",
-    projectId: "bus-mai",
-    storageBucket: "bus-mai.appspot.com",
-    messagingSenderId: "1040034863907",
-    appId: "1:1040034863907:web:02fe111a7feb0275738a96",
-    measurementId: "G-0V4WNJQSN3"
+   
 };
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
